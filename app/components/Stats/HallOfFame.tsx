84b6618c4654862d2190ffd01c620764d60499fa
0xlisamarie <0xlisamariesimpson@gmail.com>
2025-12-14 00:42:44 +0530

    fix: updated topline age hours in query

--- app/components/Stats/HallOfFame.tsx
+++ app/components/Stats/HallOfFame.tsx
@@ -12,7 +12,7 @@ const HallOfFame = () => {
   const getHistorySignalData = async () => {
     try {
       const response = await axios.get(
-        `${API_BASE_URL}/signals?force_refresh=true&min_age_hours=0&limit=10`
+        `${API_BASE_URL}/signals?force_refresh=true&min_age_hours=24&limit=10`
       );
       const signalsHistory = response?.data;
       dispatch({
